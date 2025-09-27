1c56a9a1714cdd7f645f09737357321a608353f3
강희진 <kanghuijin12@gmail.com>
2025-09-27 21:53:24 +0900

    [feat] 섹션 3: 회원 도메인 설계

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // HashMap은 동시성 문제가 발생할 수 있음 개발용으로만 사용
+    // 실무에서는 ConcurrentHashMap을 사용한다
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
