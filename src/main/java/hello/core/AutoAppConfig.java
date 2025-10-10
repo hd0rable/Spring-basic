25b7a26d416fe7d9f56f0ce1fe6da3dd0eb21217
강희진 <kanghuijin12@gmail.com>
2025-10-10 20:32:49 +0900

    [feat] 섹션 8: 옵션 처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -13,10 +13,10 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    //수동 빈 등록이 우선권을 가진다.
-    //수동 빈이 자동 빈을 오버라이딩 해버린다.
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    //수동 빈 등록이 우선권을 가진다.
+//    //수동 빈이 자동 빈을 오버라이딩 해버린다.
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
